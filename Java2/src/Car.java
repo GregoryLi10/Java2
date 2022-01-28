2ce70a787fb6a5373ac794ef18e3b1977fc7b491
GregoryLi10 <90701941+GregoryLi10@users.noreply.github.com>
2022-01-28 11:39:31 -0500

    .

--- Java2/src/Car.java
+++ Java2/src/Car.java
@@ -1,4 +1,24 @@
 
 public class Car {
-
+	private String name;
+	private int age;
+	double miles;
+	
+	public Car(String name, int age, double miles) {
+		this.name=name;
+		this.age=age;
+		this.miles=miles;
+	}
+	
+	public void drive(double distance) {
+		miles+=distance;
+	}
+	
+	public double worth() {
+		return Math.max(0, 1000-age*35-miles/50);
+	}
+	
+	public double getMiles() {
+		return miles;
+	}
 }
--- Java2/src/Garage.java
+++ Java2/src/Garage.java
@@ -1,4 +1,46 @@
 
 public class Garage {
+	public Car[] cars;
 
+	public Garage(int num) {
+		cars=new Car[num];
+	}
+	
+	public void addCar(int i, String name, int age, double miles) {
+		cars[i]=new Car(name, age, miles);
+	}
+	
+	public double totalWorth() {
+		double sum=0;
+		for (Car a:cars) {
+			if (a!=null) 
+			sum+=a.worth();
+		}
+		return sum;
+	}
+	
+	public double averageMiles() {
+		double sum=0, c=0;
+		for (Car a:cars) {
+			if (a!=null) {
+				sum+=a.getMiles();
+				c++;
+			}
+		}
+		if (c==0) return 0;
+		return sum/c;
+	}
+	
+	public void drive(int i, double n) {
+		if (cars[i]==null) return;
+		cars[i].drive(n);
+	}
+	
+	public static void main (String[] args) {
+		Garage a=new Garage(3);
+		a.addCar(0, "rock", 4, 200120);
+		a.addCar(2, "paper", 1, 3050);
+		System.out.println(a.totalWorth());
+		System.out.println(a.averageMiles());
+	}
 }
