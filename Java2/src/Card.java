1459f9b67e2aa5d97fe407386ebab34281b15e3d
GregoryLi10 <90701941+GregoryLi10@users.noreply.github.com>
2022-02-01 20:00:13 -0500

    .

---
+++ Java2/src/Card.java
@@ -0,0 +1,18 @@
+
+public class Card {
+	private String suit;
+	private int num;
+	public Card(String suit, int num) {
+		this.num=num;
+		this.suit=suit;
+	}
+	
+	public String toString() {
+		return num+" "+suit;
+	}
+	
+	public int getVal() {
+		return num;
+	}
+	
+}
