e83d4f628fc1ffa8d20d6bf7dd4b31e503230b3c
GregoryLi10 <90701941+GregoryLi10@users.noreply.github.com>
2022-02-14 21:55:00 -0500

    .

--- Java2/src/Ball/SineBall.java
+++ Java2/src/Ball/SineBall.java
@@ -4,14 +4,15 @@ import java.awt.Color;
 import java.util.ArrayList;
 
 public class SineBall extends CollisionBall{
-
+	private int startY;
 	public SineBall(int startx, int starty, int startrad, int startxspeed, int startyspeed, ArrayList<Ball> arr, Color startcolor) {
 		super(startx, starty, startrad, startxspeed, startyspeed, arr, startcolor);
+		this.startY=starty;
 	}
 	
 	public void move() {
 		setX(getX()+getXSpeed()/4);
-		setY((int) (Math.sin(getX()/20.0)*70+HEIGHT/2));
+		setY((int) (Math.sin(getX()/20.0)*70+startY));
 		super.move();
 	}
 
