353178a682957e126feac2424d727fa571909abd
GregoryLi10 <90701941+GregoryLi10@users.noreply.github.com>
2022-02-18 13:05:58 -0500

    .

--- Java2/src/pokemon/Melatonin.java
+++
@@ -1,9 +0,0 @@
-package pokemon;
-
-public class Melatonin extends Pokemon{
-
-	public Melatonin() {
-		super("Melatonin", 0, 375, "Melatonin.png", new Move[] {new Move("slep", 0, 25, 1), new Move("slep", 0, 50, 1), new Move("slep", 0, 50, 1), new Move("slep", 0, 50, 1)});
-	}
-
-}
